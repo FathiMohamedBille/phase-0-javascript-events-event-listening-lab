846fb035f39ff29fa31f399419191f12b83b567a
fathimohamed <mohafathi24@gmail.com>
2024-07-05 12:08:22 +0300

    events-event-listening-lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-function addingEventListener() {
+function addingEventListener() { 
+    const Element = document.getElementById('button');
+    if (Element) { 
+        Element.addEventListener('click', function() {
+            alert('button element was clicked!'); 
+        });
+    } else {
+        console.error('button element not found');
+    }
 }
+
+// Call the function without any arguments
+module.exports = addingEventListener;
